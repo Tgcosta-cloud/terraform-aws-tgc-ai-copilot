fffea0587e05aef5d6e611492611d7f9f75c98fd
root <tg_costa@hotmail.com>
2026-01-20 10:26:44 -0300

    feat: deploy scp

--- locals.tf
+++ locals.tf
@@ -13,4 +13,13 @@ locals {
   roles_to_enforce = local.has_enforce_roles ? [
     for role in var.ai_copilot_enforce_roles : "arn:${data.aws_partition.current.partition}:iam::*:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_${role}"
   ] : []
+
+  ai_copilot_has_default_principal = var.ai_copilot_default_group_name != "" || var.ai_copilot_default_user_name != ""
+  
+  ai_copilot_validate_principal = (
+    !var.ai_copilot_create_permission_set || 
+    length(var.ai_copilot_target_ids) == 0 || 
+    local.ai_copilot_has_default_principal
+  ) ? true : tobool("ERROR: When ai_copilot_create_permission_set=true and ai_copilot_target_ids is not empty, you must specify either ai_copilot_default_group_name or ai_copilot_default_user_name")
+
 }
